caaa87a1747580c6270a267cbbacda9a7a306439
Souvik Basu <souvikbasu@gmail.com>
2018-03-17 11:56:51 +0530

    add empty router file and include router module to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {}
+    ),
     BrowserModule
   ],
   providers: [],
